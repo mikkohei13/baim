350c3fac383e2acb8d16f56b5ddb9ef693da1f37
Mikko Heikkinen <hawk@biomi.org>
2023-02-17 15:30:03 +0200

    Add logo & adjust GUI

--- app.py
+++ app.py
@@ -25,24 +25,31 @@ def validate_float(new_value):
 
 
 root = tk.Tk()
-root.geometry("600x400")
+root.geometry("600x500")
 root.title("BAIM")
-#root.configure(background="#E6E8E9")  # set the background color to light gray
+root.configure(background="#E6E8E9")
+root.option_add("*foreground", "#0A5061")
+root.option_add("*background", "#E6E8E9")
+
+# Logo
+image = tk.PhotoImage(file="baim-icon.png")
+image_label = tk.Label(root, image=image)
+image_label.pack(pady=10)
 
 # Create input fields
-analyze_directory_label = tk.Label(root, text="Directory for BirdNET files", foreground="#0A5061")
+analyze_directory_label = tk.Label(root, text="Directory for BirdNET files")
 analyze_directory_label.pack(pady=10)
-analyze_directory = tk.Entry(root, width=65)
+analyze_directory = tk.Entry(root, width=65, background="#F5F5F5")
 analyze_directory.pack()
 
 placeholder_threshold = 0.75
 threshold_value_label = tk.Label(root, text="Prediction threshold (between 0.01-0.99)")
 threshold_value_label.pack(pady=10)
-threshold_value = tk.Entry(root, validate="key", width=10, validatecommand=(root.register(validate_float), "%P"))
+threshold_value = tk.Entry(root, validate="key", width=10, validatecommand=(root.register(validate_float), "%P"), background="#F5F5F5")
 threshold_value.insert(0, placeholder_threshold)
 threshold_value.pack()
 
-run_button = tk.Button(root, text="Analyze", command=run_application)
+run_button = tk.Button(root, text="Analyze", command=run_application, background="#F5F5F5")
 run_button.pack(pady=20)
 
 # Create a Label widget to display the status message
